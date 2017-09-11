2a06bcf1b2366da1fe6134e5404b3c4c7c9f102f
Artem Artemev <artemirk@gmail.com>
2017-09-11 20:00:03 +0800

    Calculate percent

--- vz/vzproclist.py
+++ vz/vzproclist.py
@@ -12,6 +12,12 @@ def killsend(pid,s):
     #os.kill(pid, 15)
     print "\033[93m ####### Should kill {0} {1} ######### \033[0m".format(s)
 
+def gettotal():
+    with open'/proc/stat') as f:
+        ttl = f.readline().strip().split()
+        sm = int(ttl[1])+ int(ttl[2]) + int(ttl[3]) + int(ttl[4])
+    return sm
+
 
 hh = socket.gethostname()
 pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
@@ -25,23 +31,17 @@ state = 2
 io = 41
 veid = -1
 ctime = 13
-plist = []
-pd = []
+try:
+    t = gettotal()
+except Exception:
+  total = 1
+
+
 for f in pids:
   try:
     with open('/proc/{0}/stat'.format(f), 'r') as ff:
       for l in ff:
         k = l.strip().split(' ')
-        try:
-          pd.append(int(k[pid]))
-          pd.append(k[cmd])
-          pd.append(k[state])
-          pd.append(int(k[veid]))
-          pd.append(int(k[ctime]))
-          pd.append(int(k[io]))
-          plist.append(pd)
-        except Exception:
-          continue
         ip='NONE'
         with open('/etc/vz/conf/{0}.conf'.format(k[veid]), 'r') as vzconf:
           for line in vzconf:
@@ -54,7 +54,7 @@ for f in pids:
           if bb in k[cmd]:
             killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
         if (int(k[ctime]) > 100000 or int(k[io]) > 10000) and k[cmd] not in good:
-          print '{7} {6} {0} {1} {2} vz={3} cputime={4} io={5}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip,hh)
+          print '{7} {6} {0} {1} {2} vz={3} cputime={4} io={5}i {8}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip,hh,t)
           for bb in kill:
             if bb in k[cmd]:
               killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
