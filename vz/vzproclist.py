cad561f7a065958feac28ea65eb9ea2686c0e236
Artem Artemev <artemirk@gmail.com>
2017-09-11 15:27:24 +0800

    Bad names procs

--- vz/vzproclist.py
+++ vz/vzproclist.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
 # parse /proc/$i/stat for procs
 
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('file', type=argparse.FileType('r'))
-try:
-  args = parser.parse_args()
-except IOError:
-  exit(0)
+import os
+pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
 bad = ['miner']
 good = ['(mysqld)','(mongod)']
@@ -20,26 +14,31 @@ veid = -1
 ctime = 13
 plist = []
 pd = []
-for l in args.file.readlines():
-  k = l.strip().split(' ')
+for f in pids:
   try:
-    pd.append(int(k[pid]))
-    pd.append(k[cmd])
-    pd.append(k[state])
-    pd.append(int(k[veid]))
-    pd.append(int(k[ctime]))
-    pd.append(int(k[io]))
-    plist.append(pd)
-  except Exception:
-    exit(0)
-  ip='NONE'
-  with open('/etc/vz/conf/{0}.conf'.format(k[veid]), 'r') as vzconf:
-    for line in vzconf:
-      if 'IP_ADDRESS="' in line:
+    with open('/proc/{0}/stat'.format(f), 'r') as ff:
+      for l in ff:
+        k = l.strip().split(' ')
         try:
-          ip=line[12:-2].strip().split(' ')[0]
-	except Exception:
-          ip='NONE'
-  if (int(k[ctime]) > 100000 or int(k[io]) > 10000) and k[cmd] not in good:
-    print '{6} {0} {1} {2} vz={3} cputime={4} io={5}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip)
+          pd.append(int(k[pid]))
+          pd.append(k[cmd])
+          pd.append(k[state])
+          pd.append(int(k[veid]))
+          pd.append(int(k[ctime]))
+          pd.append(int(k[io]))
+          plist.append(pd)
+        except Exception:
+          continue
+        ip='NONE'
+        with open('/etc/vz/conf/{0}.conf'.format(k[veid]), 'r') as vzconf:
+          for line in vzconf:
+            if 'IP_ADDRESS="' in line:
+              try:
+                ip=line[12:-2].strip().split(' ')[0]
+      	      except Exception:
+                ip='NONE'
+        if (int(k[ctime]) > 100000 or int(k[io]) > 10000) and k[cmd] not in good:
+          print '{6} {0} {1} {2} vz={3} cputime={4} io={5}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip)
+  except Exception:
+    continue
 #need to parse CMD string 
