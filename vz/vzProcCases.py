aa65031810e90698869da301b63f368d3e3c8f61
Artem Artemev <artemirk@gmail.com>
2017-09-11 22:09:22 +0800

    Only cases

--- vz/vzProcCases.py
+++ vz/vzProcCases.py
@@ -4,13 +4,14 @@
 import os
 import socket
 import requests
+import re
 
 
 def killsend(pid,s):
     json_string = '{"string_kill": {0}}'.format(s)
     requests.post('http://mon.ispbug.ru:35001/killproc/api/', data=json_string, headers={'Content-Type': 'application/json'})
     #os.kill(pid, 15)
-    print "\033[93m ####### Should kill {0} {1} ######### \033[0m".format(s)
+    print "\033[93m ####### Should kill {0} ######### \033[0m".format(s)
 
 def gettotal():
     with open'/proc/stat') as f:
@@ -23,8 +24,7 @@ hh = socket.gethostname()
 pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
 bad = ['ArchiSteamFarm','mod-tmp','game','cryptonight','minerd','inergate','arcticcoind','multichaind']
-bad = ['core','mgrctl','filemgr']
-good = ['(aggregator)','(apache2)','(atd)','(auditd)','(bash)','(beam.smp)','(bird)','(bundle)','(clamd)','(dbus-daemon)','(dovecot)','(ezstream)','(fail2ban-server)','(freshclam)','(gitlab-mon)','(gunicorn)','(hald)','(httpd)','(icecast2)','(ihttpd)','(init)','(inotifywait)','(java)','(liquidsoap)','(master)','(monit)','(named)','(nginx)','(node_exporter)','(ntpd)','(otrs.Daemon.pl)','(packagekitd)','(php-fpm)','(postgres)','(postgres_export)','(proftpd)','(prometheus)','(python2)','(qmgr)','(radiopoint)','(redis-server)','(rsyslogd)','(salt-minion)','(searchd)','(squid)','(sshd)','(supervisord)','(systemd)','(systemd-journal)','(systemd-logind)','(terminal.exe)','(ts3server)','(tuned)','(/usr/sbin/postg)','(/usr/sbin/spamd)','(uwsgi)']
+cbad = ['(bash)','(mgrctl)','(filemgr)']
 pid = 0
 cmd = 1
 state = 2
@@ -53,11 +53,15 @@ for f in pids:
         for bb in bad:
           if bb in k[cmd]:
             killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
-        if (int(k[ctime]) > 100000 or int(k[io]) > 10000) and k[cmd] not in good:
-          print '{7} {6} {0} {1} {2} vz={3} cputime={4} io={5}i {8}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip,hh,t)
-          for bb in kill:
-            if bb in k[cmd]:
-              killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
+        if int(k[ctime]) > 100000 and k[cmd] in cbad:
+          killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
+          #print '{7} {6} {0} {1} {2} vz={3} cputime={4} io={5}i {8}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip,hh,t)
+        for bb in kill:
+          if bb in k[cmd]:
+             killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
+        ptr = re.compile("php.*_.*") 
+        if ptr.match(k[cmd]):
+          killsend(k[pid],'{0} {1} {2} {3} {4}'.format(hh,ip,k[pid],k[cmd],k[veid]))
   except Exception:
     continue
 #need to parse CMD string 
