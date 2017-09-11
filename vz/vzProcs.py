d7a5dafa91622b85938301e9f579e0794683b3c6
Artem Artemev <artemirk@gmail.com>
2017-09-12 07:40:47 +0800

    Just list of procs

---
+++ vz/vzProcs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# parse /proc/$i/stat for procs
+
+import os
+import socket
+
+
+def gettotal():
+    with open('/proc/stat') as f:
+        ttl = f.readline().strip().split()
+        sm = int(ttl[1])+ int(ttl[2]) + int(ttl[3]) + int(ttl[4])
+    return sm
+
+
+hh = socket.gethostname()
+pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+
+pid = 0
+cmd = 1
+state = 2
+io = 41
+veid = -1
+ctime = 13
+try:
+    t = gettotal()
+except Exception:
+  total = 1
+
+
+for f in pids:
+  try:
+    with open('/proc/{0}/stat'.format(f), 'r') as ff:
+      for l in ff:
+        k = l.strip().split(' ')
+        ip='NONE'
+        with open('/etc/vz/conf/{0}.conf'.format(k[veid]), 'r') as vzconf:
+          for line in vzconf:
+            if 'IP_ADDRESS="' in line:
+              try:
+                ip=line[12:-2].strip().split(' ')[0]
+              except Exception:
+                ip='NONE'
+          print '{7} {6} {0} {1} {2} vz={3} cputime={4} io={5}'.format(k[pid],k[cmd],k[state],k[veid],k[ctime],k[io],ip,hh)
+  except Exception:
+    continue
+#need to parse CMD string 
