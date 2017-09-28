b187b36e11f5473e0c31b59b30a6aa1bba0293fa
Artem Artemev <artemirk@gmail.com>
2017-09-28 12:49:02 +0800

    File path chenged

--- vmmgr/checknodes.py
+++ vmmgr/checknodes.py
@@ -6,43 +6,13 @@ import requests
 import json
 import urllib.parse
 import re
-import sqlite3
 
-
-DB_STRING = "/opt/db/isitOk.sqlite"
-
-def read_authfile(path):
-    with open(path, 'r') as f:
-        return f.read().strip()
-
-
-def changesend(s):
-    json_string = {}
-    json_string['mem_info'] = s
-    r = requests.post('http://mon.ispbug.ru:35000/killproc/mem/api/', data=json.dumps(json_string), headers = {'Content-type': 'application/json', 'Authorization': 'Token {}'.format(read_authfile('/opt/ansible/artem/tools/vmmgr/auth'))})
-    return True
-
-def getMasterNode(host):
-     master = host.split('-')[0]
-     if master == 'msk':
-         master = "{}-{}".format(host.split('-')[0],host.split('-')[1])
-     if master == 'wkvm':  master = 'kvm'
-     return master
-def decreaseLimit(host,elid,lmt):
-     master = getMasterNode(host)
-     stdout, stderr = Popen(['ssh', '-q','-o UserKnownHostsFile=/dev/null ','-o StrictHostKeyChecking=no','-o ConnectTimeout=10', 'root@{}'.format(master), '/usr/local/mgr5/sbin/mgrctl -m vmmgr vmhostnode.edit  elid={} maxvmcount={} sok=ok'.format(elid,lmt)],stdout=PIPE,universal_newlines=True).communicate()
-     return stdout
-
-def touchtCount(host,count):
-    with sqlite3.connect(DB_STRING) as c:
-        r = c.execute("INSERT INTO countlog(name,cluster,count) VALUES (?,?,?)",['meminfo80pcnt',host,count])
-    return r
 
 
 if __name__ == '__main__':
     output = subprocess.run("ansible kvmaster -i /opt/ansible/inventory.py -m shell -a '/usr/local/mgr5/sbin/mgrctl -m vmmgr vmhostnode'  | egrep 'meminfo=[8-9][0-9]'", shell=True, stdout=subprocess.PIPE,universal_newlines=True) 
     pctCount = {}
-    fout = ""
+    f = open('/var/tmp/node80pctKVM', 'w')
     for l in str(output.stdout).split('\n'):
         vls = {}
         for vv in l.strip().split(' '):
@@ -57,7 +27,7 @@ if __name__ == '__main__':
                 pctCount[master] = 0
             else:
                 pctCount[master] += 1
-            fout += "name:{} vms:{} limit:{} mem:{}\n".format(vls['name'],vls['countvm'],vls['maxvmcount'],vls['meminfo'])
+            f.write("name:{} vms:{} limit:{} mem:{}\n".format(vls['name'],vls['countvm'],vls['maxvmcount'],vls['meminfo']))
             if vls['name'].startswith('jupiter'):
                 continue
             #changesend("{} vms:{} limit:{} mem:{}".format(vls['name'],vls['countvm'],vls['maxvmcount'],vls['meminfo']))
@@ -69,5 +39,3 @@ if __name__ == '__main__':
             changesend("{} vms:{} limit/old:{}/{} mem:{} output:{}".format(vls['name'],vls['countvm'],nlimit,vls['maxvmcount'],vls['meminfo'],out))
     for k in pctCount.keys():
         touchtCount(k,pctCount[k])
-    f = open('/var/tmp/artemcheck/node80pctKVM', 'w')
-    f.write(fout)
