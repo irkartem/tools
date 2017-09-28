4b8c69a9736003663b6b6e5304ad000eb68336d9
Artem Artemev <artemirk@gmail.com>
2017-09-28 10:09:09 +0800

    Fix output bugs

--- vmmgr/listOStmp.py
+++ vmmgr/listOStmp.py
@@ -8,19 +8,19 @@ hh = socket.gethostname()
 if os.path.isdir("/vm/nfsshare/"):
     for dirname in os.listdir('/vm/nfsshare/'):
         if dirname == 'iso': continue
-        pth = "/vm/nfsshare/{}".format(dirname)
+        pth = "/vm/nfsshare/{0}".format(dirname)
         if os.path.isdir(pth):
             try:
-                f = open("{}/metainfo.xml",'r')
+                f = open("{0}/metainfo.xml",'r')
             except Exception:
-                print("{} {} broken".format(hh,dirname))
+                print("{0} {1} broken".format(hh,dirname))
                 continue
         for line in f.readlines():
             if line.startswith('  <osname>'):
                 os = line.split('>')[1].split('<')[0]
             if line.startswith('  <version>'):
                 ver = line.split('>')[1].split('<')[0]
-        print("{} {} {}".format(hh,os,ver))
+        print("{0} {1} {2}".format(hh,os,ver))
 if os.path.isdir("/vz/template/cache/"):
-    for dirname in os.listdir('/vm/nfsshare/'):
-        print("{} {} {}".format(hh,dirname,'0'))
+    for dirname in os.listdir('/vz/template/cache/'):
+        print("{0} {1} {2}".format(hh,dirname,'0'))
