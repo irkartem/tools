030670066f8e72a7f4a0602cc08b03d079439e70
Artem Artemev <artemirk@gmail.com>
2017-10-04 13:58:50 +0800

    Debug mode

--- vz/getBad.py
+++ vz/getBad.py
@@ -14,9 +14,10 @@ DB_STRING = "/opt/db/tickets.sqlite"
 
 def killsend(s):
     json_string = {}
-    json_string['string_kill'] = s.strip()
+    json_string['string_kill'] = s.strip().replace('\x00','')
     r = requests.post('http://mon.ispbug.ru:35000/killproc/api/', data=json.dumps(json_string), headers = {'Content-type': 'application/json', 'Authorization': 'Token dd798c14e26b32c517ca2dd40c372dd4f027ba50'})
     print(r)
+    print(json_string)
     if r.status_code != 200:
         f = open('/var/tmp/artemcheck/error-procsforkill', 'a')
         f.write(" {} {}\n".format(datetime.date.today(),s))
