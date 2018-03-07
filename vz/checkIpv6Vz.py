483009f9cf71975f4dbd49a361a2cd7c7da2a35c
root <artem@ispserver.com>
2018-03-07 10:21:15 +0300

    Check commit

--- vz/checkIpv6Vz.py
+++ vz/checkIpv6Vz.py
@@ -29,18 +29,21 @@ def sendinflux(jdata):
 
 def touchMon(s,st={}):
     ath = read_authfile("/opt/token")
-    script = SenderStateScript(token=ath, name_script=s, timeout_fall_hour=8)
+    script = SenderStateScript(token=ath, name_script=s, timeout_fall_hour=13)
     script.send_all_stat(st)
 
 
 if __name__ == '__main__':
     res = response('http://mon.hoztnode.net/inventory.txt')
+    touchMon('Cron_ipv6_broken',{'status':'start'})
     outj = json.loads(res)
     nodes = outj['vznode']
     if len(nodes) < 2:
         sys.exit("Empty list of processingmodules {}".format(nodes))
     tout = 'cron:check IPv66666 \n'
-    sendInfo ={}
+    sendInfo = {}
+    sendInfo['error'] = ''
+    sendInfo['broken'] = []
     for hn in nodes:
         if len(hn) < 3:
             continue
@@ -52,11 +55,15 @@ if __name__ == '__main__':
         out = str(output.stdout).split('\n')
         if out[1] != '0':
             tout += "IPv6 BROKEN {}\n".format(hn)
-            sendInfo[hn] = 'broken'
+            sendInfo['broken'].append(hn)
+    sendInfo['count_broken'] = len(sendInfo['broken'])
     print(tout)
     if len(tout) > 30:
        r = requests.post('http://mon.ispsystem.net/telegram_senderart.py', data={'text':tout})
        requests.post('http://mon.ispsystem.net/telegram_sender.py', data={'type':'group','text':tout})
        if r.status_code > 250:
            print("can't sent telegram data {} {}".format(r, tout))
+    print(sendInfo)
+    sendInfo['status'] = 'end'
     touchMon('Cron_ipv6_broken',sendInfo)
+
